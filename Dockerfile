b1e6f3be5b7a32a76c3dccca782b967c85c2c260
ofesquiro <nicolas.esquiro@gmail.com>
2024-05-08 18:05:03 -0300

    8/5

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM python
 workdir /app
 copy . .
-RUN pip3 install speechRecognition
+RUN apt upgrade
 RUN apt update
+RUN pip3 install speechRecognition
 RUN apt install portaudio19-dev -y
 RUN apt-get install python3-pyaudio
 RUN pip3 install pyaudio
 RUN pip3 install pocketsphinx
+RUN pip3 install pygame
 
-CMD ["python", "src/inputDevice.py"]
+CMD ["python", "src/test.py"]
---
+++ src/test.py
@@ -0,0 +1,44 @@
+import speech_recognition as sr
+import pygame
+import pyaudio as pa
+RECOGNIZER = sr.Recognizer()
+def capture_voice_input():
+    with sr.Microphone() as source:
+        audio = RECOGNIZER.listen(source)
+    return audio
+
+
+
+def convert_voice_to_text(audio):
+    try:
+        text = RECOGNIZER.recognize_google_cloud(audio)
+        print("text2 dice: " + text)
+
+    except sr.UnknownValueError:
+        text = ""
+        print("Sorry, I didn't understand that.")
+    except sr.RequestError as e:
+        text = ""
+        print("Error; {0}".format(e))
+    return text
+def isBul(text):
+    if text == "bool" or text == "bul":
+        file = "livinLaVidaLoca.mp3"
+        play_music(file)
+def play_music(file):
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+
+# Replace 'your_file.mp3' with the path to your MP3 file
+
+def test():
+    audio = capture_voice_input()
+    text = convert_voice_to_text(audio)
+    isBul(text)
+
+def main():
+    test()
+main()
+
