79cd35b45cd8569f9c10fa8d853235960507f66b
BautiMarin <motomell.simpson@gmail.com>
2024-05-13 21:18:48 -0300

    FUNCIONA EL AUDIOgit add .git add . UPSIDE INSIDE OUTgit add .

--- src/main.py
+++ src/main.py
@@ -5,13 +5,18 @@ RECOGNIZER = sr.Recognizer()
 def capture_voice_input():
     with sr.Microphone() as source:
         print("listening...")
-        audio = RECOGNIZER.listen(source)
+        try:
+            audio = RECOGNIZER.listen(source,timeout=2)
+        except sr.WaitTimeoutError:
+            pass
     return audio
 
 
 
 def convert_voice_to_text(audio):
     try:
+        if(audio == None):
+            return ""
         print("converting...")
         text = RECOGNIZER.recognize_sphinx(audio,language="es-ES")
         print("text2 dice: " + text)
@@ -24,11 +29,11 @@ def convert_voice_to_text(audio):
         print("Error; {0}".format(e))
     return text
 def isBool(text):
-    if text == "hola":
+    if text=="EN PELOTAS MUY EN BOLAS":
         play_music()
 def play_music():
     print("playing la vida loca (bal)")
-    file_path = "livinLaVidaLoca.mp3"
+    file_path = "/home/alumno/Documentos/OuijaRobot/src/livinLaVidaLoca.mp3"
     pygame.mixer.init()
     pygame.mixer.music.load(file_path)
     pygame.mixer.music.play()
