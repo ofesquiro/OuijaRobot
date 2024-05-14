ffe92fd6c9691b6a077c81f47b0d12163fe7aff7
ofesquiro <nicolas.esquiro@gmail.com>
2024-05-14 16:54:34 -0300

    14/5

--- src/Dictionary_manager.py
+++ src/Dictionary_manager.py
@@ -1,19 +1,14 @@
 from enum import Enum, auto
+
+
 #FILE_PATH = "C:\ ".strip()+"Users\Hp\AppData\Local\Programs\Python\Python312\Lib\site-packages\speech_recognition\pocketsphinx-data\es-ES\pronounciation-dictionary.dict"
-FILE_PATH = "../models/es-ES/pronounciation-dictionary-copy.dict"
+FILE_PATH = "/home/esquiro/Escritorio/OuijaRobot/models/es-ES/pronounciation-dictionary-copy.dict"
 
 
-class LectureMode(Enum):
-    read_only = "r"
-    read_write = "r+"
-    append = "a"
-    write_only = "w"
-
-
-def read_dictionary(open_mode,encoding='utf-8'):
+def read_dictionary(encoding='utf-8'):
     try:
-        with open(FILE_PATH, open_mode, encoding=encoding) as file:
-            return file
+        with open(FILE_PATH,"r", encoding=encoding) as file:
+            return file.readlines()
     except FileNotFoundError:
         print("File not found.")
         return None
@@ -35,18 +30,35 @@ def correct_format_for_dictionary(text):
     return True
 
 
-def add_phrase(text, pronunciation):
-    file = read_dictionary(LectureMode.append.value)
-    if correct_format_for_dictionary(text):
+def exists_phrase(text):
+    fileLines = read_dictionary()
+    for line in fileLines:
+        if text in line:
+            print("The phrase already exists in the dictionary.")
+            return True
+    return False
+
+
+def split_text(text):
+    splited = text.split(" ")
+    return splited
+
+
+def add_phrase(text):
+    texts = split_text(text)
+    for word in texts:
+        pronunciation = input("pronunciation of " + word + ":")
+        add_one_phrase(word, pronunciation)
+
+def add_one_phrase(text, pronunciation):
+     if correct_format_for_dictionary(text) and not exists_phrase(text):
         dictionary_format_string = text + " " + pronunciation
-        file.write(dictionary_format_string)
-        content = file.read()
-        print(content)
-
-
+        with open(FILE_PATH, "a") as file:
+            file.write( "\n"+dictionary_format_string + "\n")
+    
 def remove_phrase(nonwanted_line):
     try:
-        file = read_dictionary(LectureMode.read_write.value)
+        file = read_dictionary()
         file_lines = file.readlines()
         # Remove the string from the content
         modified_lines = [line for line in file_lines if line != nonwanted_line]
@@ -56,7 +68,3 @@ def remove_phrase(nonwanted_line):
         print("File not found.")
     except Exception as e:
         print("An error occurred:", e)
-
-
-def main():
-    add_phrase("sango","s a n g o")
--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import speech_recognition as sr
 import pygame
+from Dictionary_manager import *
 
-file_path = "/home/alumno/Documentos/OuijaRobot/src/livinLaVidaLoca.mp3"
+
+file_path = "/home/esquiro/Escritorio/OuijaRobot/src/main.py"
+
 RECOGNIZER = sr.Recognizer()
 
 
@@ -9,26 +12,21 @@ def capture_voice_input():
     with sr.Microphone() as source:
         print("listening...")
         try:
-<<<<<<< HEAD
             audio = RECOGNIZER.listen(source, timeout=2)
+            raw_data = audio.get_raw_data()
+
         except sr.WaitTimeoutError:
             print("timeout")
             return None
-=======
-            audio = RECOGNIZER.listen(source,timeout=2)
         except sr.WaitTimeoutError:
-            pass
->>>>>>> 8a9519445e41027286922b47240332dca3a2fa8c
+            print("timeout")
+            return None
     return audio
 
 
 def convert_voice_to_text(audio):
     try:
-<<<<<<< HEAD
         if audio is None:
-=======
-        if(audio == None):
->>>>>>> 8a9519445e41027286922b47240332dca3a2fa8c
             return ""
         print("converting...")
         text = RECOGNIZER.recognize_sphinx(audio, language="es-ES")
@@ -41,34 +39,42 @@ def convert_voice_to_text(audio):
         text = ""
         print("Error; {0}".format(e))
     return text
-<<<<<<< HEAD
 
 
 def give_response(text):
     return text
 
 
-def play_music():
-    print("playing la vida loca (bal)")
-=======
-def isBool(text):
-    if text=="EN PELOTAS MUY EN BOLAS":
+def play(text):
+    if text == "hola":
         play_music()
+    
+    
+        
 def play_music():
     print("playing la vida loca (bal)")
-    file_path = "/home/alumno/Documentos/OuijaRobot/src/livinLaVidaLoca.mp3 "
->>>>>>> 8a9519445e41027286922b47240332dca3a2fa8c
     pygame.mixer.init()
-    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.load("src/livinLaVidaLoca.mp3")
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy():
         pygame.time.Clock().tick(10)
 
 
+def learning():
+    exit = True
+    while exit:
+        audio = capture_voice_input()
+        text = convert_voice_to_text(audio)
+        play(text)
+        feedback = input("es esto lo que quisiste decir? (s/n)")
+        if (feedback == "n"):
+            frase = input("que quisiste decir?")
+            add_phrase(frase)
+        exit = input("quieres salir? (s/n)") == "n"
+
+
 def main():
-    audio = capture_voice_input()
-    text = convert_voice_to_text(audio)
-    give_response(text)
+    learning()
 
 
 main()
