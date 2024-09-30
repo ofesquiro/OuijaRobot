67fac33882d2ed4723060769c5af6e0f48dbe501
BautiMarin <motomell.simpson@gmail.com>
2024-09-30 16:26:55 -0300

    IA DOWN EN LA TERMINAL

--- ApiCaller.py
+++ ApiCaller.py
@@ -1,57 +1,47 @@
 import requests
 import json
 
-class Usage():
-    prompt_tokens : int
-    completion_tokens : int
-    total_tokens : int
 
-
-class Choices():
-    text : str
-    usage : Usage
-
-class ResponseStruct():
-    id: str
-    object: str
-    created: int
-    model: str
-    choices: dict[Choices]
 
 # Configura tus credenciales
-api_key = '88027e88e840432a9a4a4dd479076cae'
-endpoint = 'https://lapo.openai.azure.com/'  # Reemplaza con tu endpoint
+api_key = '10af2bc76516412fac750726fa076f73'
+endpoint = 'https://perloo.openai.azure.com/'  # Reemplaza con tu endpoint
 
 # Define la ruta de la API
-model = "gpt-" # o el modelo que estés usando
-api_version = "2023-05-15"  # Verifica la versión más reciente en la documentación
+model = "gpt-4o-mini (version:2024-07-18)" 
+api_version = "2023-05-15" 
 
 # Define la URL de la API
 url = f"{endpoint}/openai/deployments/{model}/completions?api-version={api_version}"
+CONSIGNA : str = "tenes que asumir el rol de un espiritu que fue invocado en una Ouija y tenes que responder las preguntas de los jugadores de forma misteriosa, enigmatica y no muy extensa"
 
-# Configura los encabezados
-headers = {
-    'Content-Type': 'application/json',
-    'api-key': api_key,
-}
-mensaje_inicial : str = "Que hora es"
-# Cuerpo de la solicitud
-data = {
-    "prompt": mensaje_inicial,
-    "max_tokens": 50,
-}
-
-# Realiza la solicitud
-response = requests.post(url, headers=headers, json=data)
-
-# Procesa la respuesta
-if response.status_code == 200:
-    response_data : ResponseStruct = response.json()
-    print("Respuesta de la API:", response_data)
-else:
-    print("Error en la solicitud:", response.status_code, response.text)
+def enviar_request(prompt: str):
+    headers = {
+        'Content-Type': 'application/json',
+        'api-key': api_key,
+    }
+    data = {
+        "prompt": prompt,
+        "max_tokens": 50,
+    }
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 200:
+        response_data  = response.json()
+        response_text = response_data.get('choices')[0].get('text')
+        print("Respuesta de la API:", response_text)
+    else:
+        print("Error en la solicitud:", response.status_code, response.text)
 
 
+def main():
+    enviar_request(CONSIGNA)
+    while True:
+        prompt = input("Ingrese un texto: ")
+        if prompt == "exit":
+            break
+        enviar_request(prompt)
+
+main()
 """
 formato de respuesta 
 
